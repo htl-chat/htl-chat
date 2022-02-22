82b4dd85fb913c53ffad695b580332ac46c0c9cb
erias04 <github.eliascerne@icloud.com>
2022-02-22 22:25:42 +0100

    feat: added configuration

--- apps/home/tailwind.config.js
+++ apps/home/tailwind.config.js
@@ -12,6 +12,28 @@ module.exports = {
       fontFamily: {
         sans: ['Poppins', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'primaryColor': '#050F1E',
+        'countryCode': '#989DA3',
+      },
+      maxWidth: {
+        'header': '1200px',
+      },
+      height: {
+        'header': '3rem',
+      },
+      width: {
+        '7.5': '1.8rem',
+      },
+      fontSize: {
+        'xxs': '.6rem',
+      },
+      dropShadow: {
+        '3xl': '0 -3px 10px rgba(0, 0, 0, 0.65)',
+      },
     },
   },
   plugins: [],
