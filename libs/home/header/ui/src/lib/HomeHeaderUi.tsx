7be72236dda24668733348bbb39d295e5c91486d
erias04 <github.eliascerne@icloud.com>
2022-02-23 17:23:06 +0100

    init: initialize new nx workspace

---
+++ libs/home/header/ui/src/lib/HomeHeaderUi.tsx
@@ -0,0 +1,14 @@
+import './HomeHeaderUi.module.css';
+
+/* eslint-disable-next-line */
+export interface HomeHeaderUiProps {}
+
+export function HomeHeaderUi(props: HomeHeaderUiProps) {
+  return (
+    <div>
+      <h1>Welcome to HomeHeaderUi!</h1>
+    </div>
+  );
+}
+
+export default HomeHeaderUi;
