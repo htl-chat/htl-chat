0017c39b7614d15ffcfe75e45b912ce87c97ff6f
erias04 <github.eliascerne@icloud.com>
2022-02-27 19:29:29 +0100

    feat: added jit in tailwind.config.css This mode enables 'real time' compiling, called 'just in time'

--- apps/home/tailwind.config.js
+++ apps/home/tailwind.config.js
@@ -3,6 +3,7 @@ const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
 const { join } = require('path');
 
 module.exports = {
+  mode: 'jit',
   content: [
     join(__dirname, 'pages/**/!(*.stories|*.spec).{ts,tsx,html}'),
     ...createGlobPatternsForDependencies(__dirname),
