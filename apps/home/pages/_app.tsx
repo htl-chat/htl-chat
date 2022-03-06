99201107b8b40480a8e9834b0e172cb967bcb55b
erias04 <github.eliascerne@icloud.com>
2022-03-06 21:01:09 +0100

    feat: changed Head title

--- apps/home/pages/_app.tsx
+++ apps/home/pages/_app.tsx
@@ -6,7 +6,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to home!</title>
+        <title>htl.chat - A place to ...</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
