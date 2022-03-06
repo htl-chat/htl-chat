b9622ae9f7d834554234bd523f9c14a18b05bc7a
erias04 <github.eliascerne@icloud.com>
2022-03-06 16:53:47 +0100

    feat: toggle nav_menu to open

--- libs/home/header/ui/src/lib/HomeHeaderUi.tsx
+++ libs/home/header/ui/src/lib/HomeHeaderUi.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useState } from 'react';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { ReactComponent as Logo } from 'apps/home/public/logo.svg';
 import NavMenu from './nav-menu/nav-menu';
@@ -7,11 +8,16 @@ import { AiOutlineAppstore } from 'react-icons/ai';
 import './HomeHeaderUi.module.css';
 
 /* eslint-disable-next-line */
-export interface HomeHeaderUiProps {}
+export interface HomeHeaderUiProps {
+  setIsSearchOpen: any;
+}
 
 export function HomeHeaderUi(props: HomeHeaderUiProps) {
+  const { setIsSearchOpen } = props;
+  const [navMenuOpen, setNavMenuOpen] = useState(false);
+
   return (
-    <header className="w-full fixed bottom-0 left-0 z-100 bg-primaryColor">
+    <header className="w-full fixed bottom-0 left-0 z-1000 bg-primaryColor">
       <nav className="max-w-header ml-6 mr-5 h-header flex justify-between items-center text-white">
         <Link href="/">
           <div className="cursor-pointer flex gap-1.5 items-center">
@@ -27,13 +33,20 @@ export function HomeHeaderUi(props: HomeHeaderUiProps) {
         <div className="flex items-center cursor-pointer">
           <Link href="/">
             <div className="px-2.5 py-1 border-[1px] border-white rounded-3xl text-sm font-semibold mr-3">
-            Open Chat
-          </div>
+              Open Chat
+            </div>
           </Link>
-          <AiOutlineAppstore className="text-xl" />
+          <AiOutlineAppstore
+            className="text-xl"
+            onClick={() => setNavMenuOpen(true)}
+          />
         </div>
       </nav>
-      <NavMenu />
+      <NavMenu
+        navMenuOpen={navMenuOpen}
+        setNavMenuOpen={setNavMenuOpen}
+        setIsSearchOpen={setIsSearchOpen}
+      />
     </header>
   );
 }
