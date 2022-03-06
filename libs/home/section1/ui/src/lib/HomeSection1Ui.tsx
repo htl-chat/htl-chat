07be1e7ab78f36d0cf48de127eda9892045591af
erias04 <github.eliascerne@icloud.com>
2022-03-06 23:56:49 +0100

    feat: added the heading to the HomeScreen

--- libs/home/section1/ui/src/lib/HomeSection1Ui.tsx
+++ libs/home/section1/ui/src/lib/HomeSection1Ui.tsx
@@ -1,13 +1,22 @@
 import './HomeSection1Ui.module.css';
+import { Fragment } from 'react';
 
 /* eslint-disable-next-line */
 export interface HomeSection1UiProps {}
 
 export function HomeSection1Ui(props: HomeSection1UiProps) {
   return (
-    <div>
-      <h1>Welcome to HomeSection1Ui!</h1>
-    </div>
+    <Fragment>
+      <div className="text-titleColor font-black text-5xl pt-24 space-y-2">
+        <h1>A place to</h1>
+        <h1 className="text-transparent bg-clip-text w-max bg-gradient-to-r from-[#A2FACF] to-[#64ACFF]">
+          Collaborate
+        </h1>
+      </div>
+      <h2 className="font-semibold text-xl text-titleColor/50 pt-2">
+        Connect with other Students
+      </h2>
+    </Fragment>
   );
 }
 
