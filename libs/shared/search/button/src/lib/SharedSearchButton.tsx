bf5cd960a7182bf3b7b17f23712c3caf103aeb5b
erias04 <github.eliascerne@icloud.com>
2022-03-06 16:54:27 +0100

    feat: added button open functionality

--- libs/shared/search/button/src/lib/SharedSearchButton.tsx
+++ libs/shared/search/button/src/lib/SharedSearchButton.tsx
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 import { FiSearch, FiCommand } from 'react-icons/fi';
 
 import './SharedSearchButton.module.css';
 
 /* eslint-disable-next-line */
-export interface SharedSearchButtonProps {}
+export interface SharedSearchButtonProps {
+  setIsSearchOpen: any;
+  setNavMenuOpen: any;
+}
 
 export function SharedSearchButton(props: SharedSearchButtonProps) {
+  const { setIsSearchOpen, setNavMenuOpen } = props;
+
+  function toggleModals() {
+    // Close nav_menu when clicking onto the search button to not cause an overflow error
+    setNavMenuOpen(false);
+    setIsSearchOpen(true);
+  }
+
   return (
-    <button className="bg-searchBarBg border-searchBarBorder border-2 pl-4 py-1 pr-2 rounded-xl cursor-pointer w-full">
+    <button
+      onClick={toggleModals}
+      className="bg-searchBarBg border-searchBarBorder border-2 pl-4 py-1 pr-2 rounded-xl cursor-pointer w-full"
+    >
       <div className="flex justify-between text-searchBarText">
         <div className="flex items-center gap-2">
           <FiSearch className="text-xl" />
