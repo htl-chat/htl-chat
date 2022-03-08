979f33da278ca54c49c88cf88636e72e8dcb585c
erias04 <github.eliascerne@icloud.com>
2022-03-08 09:17:02 +0100

    feat: styled the HeroGlow in section1

--- libs/home/section1/ui/src/lib/HomeSection1Ui.tsx
+++ libs/home/section1/ui/src/lib/HomeSection1Ui.tsx
@@ -1,5 +1,6 @@
 import './HomeSection1Ui.module.css';
 import { Fragment } from 'react';
+import { ReactComponent as HeroGlow } from '../assets/hero-glow.svg';
 
 /* eslint-disable-next-line */
 export interface HomeSection1UiProps {}
@@ -7,7 +8,7 @@ export interface HomeSection1UiProps {}
 export function HomeSection1Ui(props: HomeSection1UiProps) {
   return (
     <Fragment>
-      <div className="text-titleColor font-black text-5xl pt-24 space-y-2">
+      <div className="text-titleColor font-black text-5xl pt-24 space-y-2 z-10">
         <h1>A place to</h1>
         <h1 className="text-transparent bg-clip-text w-max bg-gradient-to-r from-[#A2FACF] to-[#64ACFF]">
           Collaborate
@@ -16,6 +17,9 @@ export function HomeSection1Ui(props: HomeSection1UiProps) {
       <h2 className="font-semibold text-xl text-titleColor/50 pt-2">
         Connect with other Students
       </h2>
+      <div className="absolute top-[60%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <HeroGlow className="w-[850px]" />
+      </div>
     </Fragment>
   );
 }
