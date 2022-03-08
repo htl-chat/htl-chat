0675645f3438a818a25830b4764e521922f405b9
erias04 <github.eliascerne@icloud.com>
2022-03-08 10:04:11 +0100

    feat: added HomeEarthUi

--- libs/home/section1/ui/src/lib/HomeSection1Ui.tsx
+++ libs/home/section1/ui/src/lib/HomeSection1Ui.tsx
@@ -1,6 +1,7 @@
 import './HomeSection1Ui.module.css';
 import { Fragment } from 'react';
 import { ReactComponent as HeroGlow } from '../assets/hero-glow.svg';
+import { HomeEarthUi } from '@htl-chat/earth';
 
 /* eslint-disable-next-line */
 export interface HomeSection1UiProps {}
@@ -8,6 +9,9 @@ export interface HomeSection1UiProps {}
 export function HomeSection1Ui(props: HomeSection1UiProps) {
   return (
     <Fragment>
+      <div className="absolute top-[60%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+        <HeroGlow className="w-[850px]" />
+      </div>
       <div className="text-titleColor font-black text-5xl pt-24 space-y-2 z-10">
         <h1>A place to</h1>
         <h1 className="text-transparent bg-clip-text w-max bg-gradient-to-r from-[#A2FACF] to-[#64ACFF]">
@@ -17,8 +21,8 @@ export function HomeSection1Ui(props: HomeSection1UiProps) {
       <h2 className="font-semibold text-xl text-titleColor/50 pt-2">
         Connect with other Students
       </h2>
-      <div className="absolute top-[60%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <HeroGlow className="w-[850px]" />
+      <div className="relative top-12">
+        <HomeEarthUi />
       </div>
     </Fragment>
   );
