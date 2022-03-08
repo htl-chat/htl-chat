0a0e3bdad1cf7dbb77e472af7874dad3ece80451
erias04 <github.eliascerne@icloud.com>
2022-03-08 09:16:32 +0100

    feat: allowed children for HomeMain and added a relative position

--- libs/home/main/src/lib/HomeMain.tsx
+++ libs/home/main/src/lib/HomeMain.tsx
@@ -3,11 +3,14 @@ import { HomeSection1Ui } from '@htl-chat/home/section1/ui';
 import './HomeMain.module.css';
 
 /* eslint-disable-next-line */
-export interface HomeMainProps {}
+export interface HomeMainProps {
+  children: JSX.Element|JSX.Element[];
+}
 
 export function HomeMain(props: HomeMainProps) {
   return (
-    <main className="bg-primaryColor w-screen h-screen px-6">
+    <main className="relative bg-primaryColor w-screen h-screen px-6 overflow-x-hidden">
+      {props.children}
       <HomeSection1Ui />
     </main>
   );
