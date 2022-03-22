d519a8bab76596642049bec2b3e8e3f57e45921e
erias04 <github.eliascerne@icloud.com>
2022-03-22 20:00:44 +0100

    feat: added a threejs renderer for the globe

--- libs/home/earth/renderer/src/lib/HomeEarthRenderer.tsx
+++ libs/home/earth/renderer/src/lib/HomeEarthRenderer.tsx
@@ -1,14 +1,45 @@
+import React from 'react';
+import * as ViewGL from '@htl-chat/earth-ui';
+
 import './HomeEarthRenderer.module.css';
 
 /* eslint-disable-next-line */
 export interface HomeEarthRendererProps {}
 
-export function HomeEarthRenderer(props: HomeEarthRendererProps) {
-  return (
-    <div>
-      <h1>Welcome to HomeEarthRenderer!</h1>
-    </div>
-  );
+export class HomeEarthRenderer extends React.Component<HomeEarthRendererProps> {
+  private canvasRef: React.RefObject<HTMLCanvasElement>;
+  viewGL: any;
+
+  constructor(props: any) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
+  // ******************* COMPONENT LIFECYCLE ******************* //
+  override componentDidMount() {
+    const canvas = this.canvasRef.current;
+    this.viewGL = new ViewGL(canvas);
+
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  override componentDidUpdate() {}
+
+  override componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize = () => {
+    this.viewGL.onWindowResize(window.innerWidth, window.innerHeight);
+  };
+
+  override render() {
+    return (
+      <div className="">
+        <canvas ref={this.canvasRef}></canvas>
+      </div>
+    );
+  }
 }
 
 export default HomeEarthRenderer;
